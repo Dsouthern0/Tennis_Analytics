47b0580cd4880592de93dd5d994bd24644343ccc
Dsouthern0 <95564697+Dsouthern0@users.noreply.github.com>
2022-01-17 09:16:37 +0000

    Update Tennis Q1.py

--- Tennis Q1.py
+++ Tennis Q1.py
@@ -2,10 +2,14 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import mplcursors
 
-df = pd.read_csv('data/tournaments_1877-2017_unindexed_csv.csv', parse_dates = ['tourney_dates'])
+grand_slam_df = pd.read_csv('data/tournaments_1877-2017_unindexed_csv.csv', parse_dates = ['tourney_dates'])
+#grand_slam = ["wimbledon", "us-open", "australian-open", "roland-garros"]
 grand_slam = ["wimbledon", "us-open", "australian-open", "roland-garros"]
-grand_slam_df = df[df["tourney_slug"].isin(grand_slam)]
+#surface = ["Grass", "Hard", "Carpet", "Clay"]
+surface = ["Grass", "Hard", "Carpet", "Clay"]
 
+grand_slam_df = grand_slam_df[grand_slam_df["tourney_slug"].isin(grand_slam)]
+grand_slam_df = grand_slam_df[grand_slam_df["tourney_surface"].isin(surface)]
 
 count = grand_slam_df.groupby(['singles_winner_name']).cumcount().add(1)
 
